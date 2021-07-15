b69ee5e51eba8622567f865658229f59c55ec801
Regi Fronda <regisfronda@gmail.com>
2021-07-15 02:16:03 -0700

    Add initialize method to LinkedList class

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
 class LinkedList
+  attr_accessor :head
+
+  def initialize(value)
+    @head = Node.new(value, nil)
+  end
 end
 
 class Node
   attr_accessor :value, :next_node
-  
+
   def initialize (value = nil, next_node = nil)
     @value = value
     @next_node = nil
